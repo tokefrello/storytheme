6b22b04b68cd14305390e04d3f04f52d4c7202db
tokefrello <tokefrello@gmail.com>
2017-01-05 16:35:09 +0100

    add pseudo code

--- sketch.js
+++ sketch.js
@@ -1,16 +1,37 @@
+//initialize Figures array
+//define background and Figure color
+//initialize GUI
+//initialize sounds
+
 function setup() {
 	createCanvas(displayWidth, displayHeight);
+
+	//populate Figures array
 }
 
 function draw() {
+	//background color
+	//update Figures
+	//display Figures
+	//play sounds
+	//make GUI operations
 }
 
 //Figure class
 function Figure() {
+	//location vector
+	//velocity vector
+	//color
+	//size
 
 	this.move = function() {
+		//update velocity
+		//update location
+		//check for boundaries
 	}
 	
 	this.display = function() {
+		//register colors
+		//display shape
 	}
 };
